ba4c0f2ce76ebdb7e35eca8c51edfaa29e6d8a1d
Luis Gaytan <a01771191@tec.mx>
2024-11-21 18:41:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+#include "Game.h"
 #include <iostream>
 
 int main() {
-    
+    Game game("Player1", 5, 5);
+    game.start();
+
+    char direction;
+    std::cout << "Welcome to the Maze Game.\n";
+
+    while (true) {
+        std::cout << "Move (w=up, s=down, a=left, d=right, q=quit): ";
+        std::cin >> direction;
+        if (direction == 'q') break;
+
+        std::string result = game.movePlayer(direction);
+        std::cout << result << '\n';
+    }
+
+    std::cout << "Thanks for playing!\n";
     return 0;
 }
