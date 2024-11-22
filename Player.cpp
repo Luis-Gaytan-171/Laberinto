2a94f084499eef1947659fbd573a2d4f22af9a59
Luis Gaytan <a01771191@tec.mx>
2024-11-21 18:34:33 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,18 +1,25 @@
-#ifndef PLAYER_H
-#define PLAYER_H
+#include "Player.h"
 
-#include "Maze.h"
+Player::Player(int x, int y) : posX(x), posY(y) {}
 
-class Player {
-public:
-    Player(int x, int y);
-    void move(char direction, const Maze& maze);
-    int getPosX() const;
-    int getPosY() const;
+void Player::move(char direction, const Maze& maze) {
+    switch (direction) {
+        case 'w':
+            if (maze.isValidMove(posX - 1, posY)) posX--;
+            break;
+        case 's':
+            if (maze.isValidMove(posX + 1, posY)) posX++;
+            break;
+        case 'a':
+            if (maze.isValidMove(posX, posY - 1)) posY--;
+            break;
+        case 'd':
+            if (maze.isValidMove(posX, posY + 1)) posY++;
+            break;
+        default:
+            break;
+    }
+}
 
-private:
-    int posX;
-    int posY;
-};
-
-#endif
+int Player::getPosX() const { return posX; }
+int Player::getPosY() const { return posY; }
